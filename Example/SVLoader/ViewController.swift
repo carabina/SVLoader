a9707e6e9ecb9c0a59d00bec321a912db6281275
Slava Semeniuk <slavasemeniuk@gmail.com>
2016-12-12 17:26:39 +0200

    Initial commit

---
+++ Example/SVLoader/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SVLoader
+//
+//  Created by Slava Semeniuk on 12/12/2016.
+//  Copyright (c) 2016 Slava Semeniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
